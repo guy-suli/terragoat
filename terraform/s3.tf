eb65c1e338b4be2201cb5324962b5f1060b7de29
Guy Sulema <48174831+guy-suli@users.noreply.github.com>
2020-12-21 12:49:35 +0200

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -86,3 +86,17 @@ resource "aws_s3_bucket" "logs" {
     Environment = local.resource_prefix.value
   }
 }
+
+
+resource "aws_s3_bucket" "logs1" {
+  bucket = "${local.resource_prefix.value}-logs"
+  acl    = "log-delivery-write"
+  versioning {
+    enabled = false
+  }
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-logs"
+    Environment = local.resource_prefix.value
+  }
+}
